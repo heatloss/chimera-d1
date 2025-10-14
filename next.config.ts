e691ce2735ebdfb46bc2b94cfe4c800aaf4d4e1a
Mike Stamm <mike@heatlossgames.com>
2025-10-13 20:18:53 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import { withPayload } from '@payloadcms/next/withPayload'
+import type { NextConfig } from 'next'
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const nextConfig: NextConfig = {
   // Your Next.js config here
   output: 'standalone', // Required for OpenNext Cloudflare builds
   webpack: (webpackConfig: any, { isServer }: any) => {
